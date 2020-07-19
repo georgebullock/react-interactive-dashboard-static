7b28d298f865f4e13f04d6c8f176676b97bb120d
George <george.bullock@gmx.de>
2020-07-19 15:02:35 +0200

    RID-001: Add JSON test fetch request. Update file name. Change script name

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ProductList from './components/ProductList/ProductList';
+import ProductList from './components/Container/Container';
 import './../src/app.scss';
 
 ReactDOM.render(<ProductList />, document.querySelector('#react-entry'));
