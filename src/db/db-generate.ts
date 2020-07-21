b1ec165e34d847950ac5de767af21357932d61cd
George <george.bullock@gmx.de>
2020-07-21 20:27:14 +0200

    RID-001: Data generation script WIP

--- src/db/db-generate.ts
+++
@@ -1,3 +0,0 @@
-import * as faker from 'faker';
-
-console.log(faker.name.firstName());
---
+++ src/helpers/db-generate.ts
@@ -0,0 +1,88 @@
+import * as faker from 'faker';
+import * as fs from 'fs';
+
+const db = {};
+const users: User[] = [];
+const comments: Comment[] = [];
+const tags: Tag[] = [];
+
+type User = {
+	id: number;
+	username: string;
+	email: string;
+	password: string;
+	created_at: string;
+};
+
+type Comment = {
+	id: number;
+	userId: number;
+	body: string;
+	created_at: string;
+};
+
+type Tag = {
+	id: number;
+	tag: string;
+	created_at: string;
+};
+
+function generateUsers(number: number): void {
+	for (let i = 0; i < number; i++) {
+		const obj = {} as User;
+
+		obj['id'] = i;
+		obj['username'] = faker.internet.userName();
+		obj['email'] = faker.internet.email();
+		obj['password'] = faker.internet.password();
+		obj['created_at'] = faker.date
+			.between(new Date(2015, 11, 17), new Date())
+			.toISOString();
+
+		users.push(obj);
+	}
+}
+
+function generateComments(number: number): void {
+	for (let i = 0; i < number; i++) {
+		const obj = {} as Comment;
+		let date: string;
+		obj['id'] = i;
+		obj['userId'] = Math.floor(Math.random() * 11);
+		obj['body'] = faker.lorem.sentence(
+			faker.random.number({ min: 1, max: 10 })
+		);
+
+		for (const user of users) {
+			if (user.id === obj.userId) {
+				date = faker.date.between(user.created_at, new Date()).toISOString();
+			}
+		}
+
+		obj['created_at'] = date;
+
+		comments.push(obj);
+	}
+}
+
+function generateTags(number: number): void {
+	for (let i = 0; i < number; i++) {
+		const obj = {} as Tag;
+		obj['id'] = i;
+		obj['tag'] = faker.lorem.word();
+		obj['created_at'] = obj['created_at'] = faker.date
+			.between(new Date(2015, 11, 17), new Date())
+			.toISOString();
+		tags.push(obj);
+	}
+}
+
+function generateDatabase(users: number, comments: number, tags: number): void {
+	db['users'] = generateUsers(users);
+	db['comments'] = generateComments(comments);
+	db['tags'] = generateTags(tags);
+
+	fs.writeFileSync('./../../src/db/db.json', JSON.stringify(db));
+}
+
+generateDatabase(10, 20, 5);
