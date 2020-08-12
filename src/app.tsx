19bb97db9566dcbbd708151c3af345fc4516bdb9
George <george.bullock@gmx.de>
2020-08-12 14:56:56 +0200

    Add index and dashboard routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,38 +1,27 @@
 import React, { ReactElement } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-type State = {
-	list: ReactElement[];
+const CommentApp = (): ReactElement => {
+	return <div>Comment App</div>;
 };
 
-class App extends React.Component<{}, State> {
-	state: State = {
-		list: []
-	};
+const Dashboard = (): ReactElement => {
+	return <div>Dashboard</div>;
+};
 
-	componentDidMount(): void {
-		fetch('http://localhost:3000/comments')
-			.then(response => response.json())
-			.then(data => {
-				const list = data.map(item => {
-					return <li key={item.id}>{item.body}</li>;
-				});
-
-				this.setState({ list });
-			})
-			.catch(error => {
-				console.error(error);
-			});
-	}
-
-	render(): React.ReactElement {
-		console.log('state: ', this.state.list);
-		return (
-			<>
-				<h1>React Interactive Dashboard</h1>
-				<ul className="ui dividing centered header">{this.state.list}</ul>;
-			</>
-		);
-	}
+function App(): ReactElement {
+	return (
+		<>
+			<Switch>
+				<Route exact={true} path="/">
+					<CommentApp />
+				</Route>
+				<Route path="/dashboard">
+					<Dashboard />
+				</Route>
+			</Switch>
+		</>
+	);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 // eslint-disable-next-line
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './../src/app.scss';
 
 ReactDOM.render(
-	<BrowserRouter>
+	<Router>
 		<App />
-	</BrowserRouter>,
+	</Router>,
 	document.querySelector('#react-entry')
 );
