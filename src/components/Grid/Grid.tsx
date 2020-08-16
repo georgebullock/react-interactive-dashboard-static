eaf08dc952464f87425c8501d097febc05f10e7b
George <george.bullock@gmx.de>
2020-08-16 16:15:46 +0200

    RID-002: Add dashboard and grid components

---
+++ src/components/Grid/Grid.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styles from './Grid.module.scss';
+
+type GridAlignItems =
+	| 'flex-start'
+	| 'flex-end'
+	| 'center'
+	| 'stretch'
+	| 'baseline';
+
+type GridJustifyContent =
+	| 'flex-start'
+	| 'flex-end'
+	| 'center'
+	| 'space-between'
+	| 'space-around'
+	| 'space-evenly';
+
+type GridColSizes = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
+interface GridProps {
+	row?: boolean;
+	column?: boolean;
+	expanded?: boolean;
+	alignItems?: GridAlignItems;
+	justify?: GridJustifyContent;
+	lg?: GridColSizes;
+	md?: GridColSizes;
+	sm?: GridColSizes;
+	children?: JSX.Element[] | JSX.Element;
+}
+
+const Grid: React.FunctionComponent<GridProps> = ({
+	row,
+	column,
+	expanded,
+	alignItems,
+	justify,
+	lg,
+	md,
+	sm,
+	children
+}: GridProps) => {
+	const isRow = row || column;
+
+	const classes: string =
+		(!isRow ? styles.column : styles.row) +
+		// Row styling
+		(isRow && expanded ? ` ${styles.expanded}` : '') +
+		(isRow && justify ? ` ${styles[justify]}` : '') +
+		(isRow && alignItems ? ` ${styles['align-' + alignItems]}` : '') +
+		// Column styling
+		(!isRow && sm ? ` ${styles['sm-' + sm]}` : '') +
+		(!isRow && md ? ` ${styles['md-' + md]}` : '') +
+		(!isRow && lg ? ` ${styles['lg-' + lg]}` : '');
+
+	return <div className={classes}>{children}</div>;
+};
+
+export default Grid;
--- src/routes/dashboard/dashboard.tsx
+++ src/routes/dashboard/dashboard.tsx
@@ -1,7 +1,20 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
+import Grid from './../../components/Grid/Grid';
+import Navigation from './../../components/Navigation/Navigation';
 
-const Dashboard = (): ReactElement => {
-	return <div>Dashboard</div>;
+const Dashboard = (): JSX.Element => {
+	return (
+		<Grid row={true}>
+			<Grid sm={12} md={1}>
+				<Navigation />
+			</Grid>
+			<Grid sm={12} md={11}>
+				<div style={{ height: 1000, backgroundColor: '#c1c1c1' }}>
+					Main Content
+				</div>
+			</Grid>
+		</Grid>
+	);
 };
 
 export default Dashboard;
