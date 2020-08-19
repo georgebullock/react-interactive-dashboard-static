839424c63c67f430a470c6132c7833b21a905d00
George <george.bullock@gmx.de>
2020-08-19 20:28:20 +0200

    RID-002: Add react-icons context provider

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import { FaHome, FaChartBar, FaSignOutAlt } from 'react-icons/fa';
+import { IconContext } from 'react-icons';
 import styles from './Navigation.module.scss';
+
+const iconStyles = {
+	color: '#5d8def',
+	size: '3rem'
+};
 
 const Navigation: React.FunctionComponent = () => {
 	return (
 		<nav className={styles.nav}>
-			<ul className={styles.list}>
-				<li className={styles['list-item']}>
-					<FaHome />
-				</li>
-				<li className={styles['list-item']}>
-					<FaChartBar />
-				</li>
-				<li className={styles['list-item']}>
-					<FaSignOutAlt />
-				</li>
-			</ul>
+			<IconContext.Provider value={{ ...iconStyles }}>
+				<ul className={styles.list}>
+					<li className={styles['list-item']}>
+						<FaHome />
+					</li>
+					<li className={styles['list-item']}>
+						<FaChartBar />
+					</li>
+					<li className={styles['list-item']}>
+						<FaSignOutAlt />
+					</li>
+				</ul>
+			</IconContext.Provider>
 		</nav>
 	);
 };
