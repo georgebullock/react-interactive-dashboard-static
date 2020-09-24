572698d11ca47872773ff95dbd8ca14b533eca95
George <george.bullock@gmx.de>
2020-09-24 15:37:17 +0200

    RID-003: Refactor navigation. Add types.

--- src/core/Navigation/Navigation.tsx
+++ src/core/Navigation/Navigation.tsx
@@ -1,31 +1,89 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { IconContext, IconType } from 'react-icons';
 import { FaHome, FaChartBar, FaSignOutAlt } from 'react-icons/fa';
-import { IconContext } from 'react-icons';
 import styles from './Navigation.module.scss';
 
+/** ==============================================
+ * Types/Interfaces
+ * ============================================== */
+type RouteObject = {
+	url: string;
+	Icon: IconType;
+	text?: string;
+};
+
+interface NavListProps {
+	routes: RouteObject[];
+}
+
+interface NavItemProps {
+	item: {
+		url: string;
+		Icon: IconType;
+		text?: string;
+	};
+}
+
+/** ==============================================
+ * Third-Party Component Props
+ * ============================================== */
+// react-icons Provider object
 const iconStyles = {
 	color: '#5d8def',
 	size: '3rem'
 };
 
+/** ==============================================
+ * Dashboard Routes
+ * ============================================== */
+const routes = [
+	{
+		url: '/',
+		Icon: FaHome
+	},
+	{
+		url: '/dashboard',
+		Icon: FaChartBar
+	},
+	{
+		url: '#',
+		Icon: FaSignOutAlt
+	}
+];
+
 const Navigation: React.FunctionComponent = () => {
 	return (
 		<nav className={styles.nav}>
 			<IconContext.Provider value={{ ...iconStyles }}>
-				<ul className={styles.list}>
-					<NavLink className={styles['list-item']} to="/">
-						<FaHome />
-					</NavLink>
-					<NavLink className={styles['list-item']} to="/dashboard">
-						<FaChartBar />
-					</NavLink>
-					<NavLink className={styles['list-item']} to="/dashboard">
-						<FaSignOutAlt />
-					</NavLink>
-				</ul>
+				<NavigationList routes={routes} />
 			</IconContext.Provider>
 		</nav>
+	);
+};
+
+const NavigationList: React.FunctionComponent<NavListProps> = ({
+	routes
+}: NavListProps) => {
+	return (
+		<ul className={styles.list}>
+			{routes.map(item => {
+				return <NavItem key={item.url} item={item} />;
+			})}
+		</ul>
+	);
+};
+
+const NavItem: React.FunctionComponent<NavItemProps> = ({
+	item
+}: NavItemProps) => {
+	const { url, Icon, text } = item;
+
+	return (
+		<NavLink className={styles['list-item']} to={url}>
+			<Icon />
+			{text}
+		</NavLink>
 	);
 };
 
