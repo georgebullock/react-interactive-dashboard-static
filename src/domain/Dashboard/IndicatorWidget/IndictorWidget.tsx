c1a61811d3d56d908ec697f036e27b3d2468832f
George <george.bullock@gmx.de>
2020-10-25 17:32:43 +0100

    RID-004: Remove unused intefaces. Remove headline. Add styles.

--- src/domain/Dashboard/IndicatorWidget/IndictorWidget.tsx
+++ src/domain/Dashboard/IndicatorWidget/IndictorWidget.tsx
@@ -1,30 +1,20 @@
 import React, { ReactElement } from 'react';
 import styles from './IndicatorWidget.module.scss';
-import Headline from './../../../shared/Headline/Headline';
 import Indicator from './../Indicator/Indicator';
-import { IconType } from 'react-icons';
 
 /** ==============================================
  * Types/Interfaces
  * ============================================== */
 
-interface IndicatorProps {
-	value: number;
-}
-
-interface ContextBarProps {
-	text: string;
-	value: number;
-	icon?: IconType;
-}
-
 const IndicatorWidget = (): ReactElement => {
 	return (
-		<div>
-			Indicator Widget
-			<Headline headingLevel="h2" headlineText="Comments" />
-			<Indicator />
-		</div>
+		<section className={styles.widget}>
+			<Indicator
+				indicatorValue="300"
+				contextBarValue="35%"
+				hasContextBar={true}
+			/>
+		</section>
 	);
 };
 
