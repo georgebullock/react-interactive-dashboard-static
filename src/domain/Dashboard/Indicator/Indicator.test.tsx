d33125cbedf15d1faa9d2de26de4e35e4d89e430
George <george.bullock@gmx.de>
2020-10-25 17:34:31 +0100

    RID-004: Add static indicator component and related styles

--- src/domain/Dashboard/Indicator/Indicator.test.tsx
+++ src/domain/Dashboard/Indicator/Indicator.test.tsx
@@ -1,27 +1,38 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Indicator from './Indicator';
+import Indicator, { IndicatorProps } from './Indicator';
 
 describe('Indicator', () => {
-	const createTestProps = props => {
+	const createTestProps = (props?: IndicatorProps): IndicatorProps => {
 		return {
 			// common props
-
+			indicatorValue: '250',
+			contextBarValue: '25%',
+			hasContextBar: true,
 			// common props override
 			...props
 		};
 	};
 
 	describe('Text rendering', () => {
-		test.skip('It should should the number of comments', () => {
+		test('It should should render the correct number of comments', () => {
 			// Arrange - Setup your test
-			const props = createTestProps({});
+			const props = createTestProps();
 
 			// Act - Take some action
-			render(<Indicator />);
+			render(
+				<Indicator
+					indicatorValue={props.indicatorValue}
+					contextBarValue={props.contextBarValue}
+					hasContextBar={props.hasContextBar}
+				/>
+			);
 
 			// Assert - Determine if something is as expected
+			expect(screen.getByText('Comments')).toBeInTheDocument();
+			expect(screen.getByRole('article')).toHaveTextContent('250');
+			expect(screen.getByText('Δ 25%')).toBeInTheDocument();
 		});
 	});
 });
--- src/domain/Dashboard/Indicator/Indicator.tsx
+++ src/domain/Dashboard/Indicator/Indicator.tsx
@@ -1,21 +1,42 @@
 import React from 'react';
+import Headline from './../../../shared/Headline/Headline';
 import styles from './Indicator.module.scss';
 
 /** ==============================================
  * Types/Interfaces
  * ============================================== */
+export interface IndicatorProps {
+	indicatorValue: string;
+	contextBarValue?: string;
+	hasContextBar?: boolean;
+}
 
-const Indicator = () => {
+export interface ContextBarProps {
+	contextBarValue?: string;
+}
+
+const Indicator: React.FunctionComponent<IndicatorProps> = ({
+	indicatorValue,
+	contextBarValue,
+	hasContextBar
+}: IndicatorProps) => {
 	return (
-		<div className={styles.indicator}>
-			{20}
-			<IndicatorContextBar />
-		</div>
+		<article className={styles.indicator}>
+			<Headline headlineText="Comments" headingLevel="h2" />
+			<div className={styles['indicator__value']}>{indicatorValue}</div>
+			{hasContextBar ? (
+				<IndicatorContextBar contextBarValue={contextBarValue} />
+			) : null}
+		</article>
 	);
 };
 
-const IndicatorContextBar = () => {
-	return <div className={styles['indicator__context-bar']}></div>;
+export const IndicatorContextBar: React.FunctionComponent<ContextBarProps> = ({
+	contextBarValue
+}: ContextBarProps) => {
+	return (
+		<div className={styles['indicator__context-bar']}>Δ {contextBarValue}</div>
+	);
 };
 
 export default Indicator;
